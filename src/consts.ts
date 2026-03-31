f15960e454bff215abf38c21f46e95d05c13e65e
sugelalei <sugelalei@sugelaleideMac-mini.local>
2026-03-31 09:00:40 +0800

    initial: sugelalei personal site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'sugelalei';
+export const SITE_DESCRIPTION = '投资 · AI · 独立思考';
